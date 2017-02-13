2e927cd7d54036993b529e343e4577bd00dc7922
burning-brush <Nicktron@Nicholass-MacBook-Pro.local>
2017-02-13 10:00:51 -0800

    added main content.php file
    simple file set up

---
+++ ntr-streamline/template-parts/content.php
@@ -0,0 +1,15 @@
+<?php
+
+?>
+<article>
+<?php
+/*
+if this is the blogroll
+then print the title, date, and category with blogroll format
+
+else if this is single, then print the heading with single format
+
+
+*/
+?>
+</article>
