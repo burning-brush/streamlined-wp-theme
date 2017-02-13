ab8004ed67006b51661679648aa834d3b5ec4dfe
burning-brush <Nicktron@Nicholass-MacBook-Pro.local>
2017-02-13 13:44:06 -0800

    Added core files
    + setup and styles to functions
    + page.php

--- ntr-streamline/function.php
+++ ntr-streamline/function.php
@@ -1,7 +1,90 @@
 <?php
 
 
+function streamline_setup() {
+  /*
+	 * Make theme available for translation.
+	 * Translations can be filed at WordPress.org. See: https://translate.wordpress.org/projects/wp-themes/twentyseventeen
+	 * If you're building a theme based on Twenty Seventeen, use a find and replace
+	 * to change 'twentyseventeen' to the name of your theme in all the template files.
+	 */
+	load_theme_textdomain( 'streamline' );
 
+  // Add default posts and comments RSS feed links to head.
+	add_theme_support( 'automatic-feed-links' );
+
+  /*
+	 * Let WordPress manage the document title.
+	 * By adding theme support, we declare that this theme does not use a
+	 * hard-coded <title> tag in the document head, and expect WordPress to
+	 * provide it for us.
+	 */
+	add_theme_support( 'title-tag' );
+
+  /*
+	 * Enable support for Post Thumbnails on posts and pages.
+	 *
+	 * @link https://developer.wordpress.org/themes/functionality/featured-images-post-thumbnails/
+	 */
+	add_theme_support( 'post-thumbnails' );
+	add_image_size( 'streamline-featured-image', 2000, 1200, true );
+	add_image_size( 'streamline-thumbnail-avatar', 100, 100, true );
+
+  // Set the default content width.
+	//$GLOBALS['content_width'] = 525;
+
+  // This theme uses wp_nav_menu() in two locations.
+	register_nav_menus( array(
+		'top'    => __( 'Top Menu', 'streamline' ),
+		'social' => __( 'Social Links Menu', 'streamline' ),
+	) );
+
+  /*
+	 * Switch default core markup for search form, comment form, and comments
+	 * to output valid HTML5.
+	 */
+	add_theme_support( 'html5', array(
+		'comment-form',
+		'comment-list',
+		'gallery',
+		'caption',
+	) );
+
+  /*
+	 * Enable support for Post Formats.
+	 *
+	 * See: https://codex.wordpress.org/Post_Formats
+	 */
+	add_theme_support( 'post-formats', array(
+		'aside',
+		'image',
+		'video',
+		'quote',
+		'link',
+		'gallery',
+		'audio',
+	) );
+
+  // Add theme support for Custom Logo.
+	add_theme_support( 'custom-logo', array(
+		'width'       => 250,
+		'height'      => 250,
+		'flex-width'  => true,
+	) );
+
+  // Add theme support for selective refresh for widgets.
+	add_theme_support( 'customize-selective-refresh-widgets' );
+
+  /*
+	 * This theme styles the visual editor to resemble the theme style,
+	 * specifically font, colors, and column width.
+ 	 */
+	//add_editor_style( array( 'assets/css/editor-style.css', twentyseventeen_fonts_url() ) );
+
+
+
+}
+add_action( 'after_setup_theme', 'streamline-setup')
 
 /**
  * Actions hooks
@@ -23,3 +106,12 @@ function sl_after_content_setup(){
 <?php
 }
 add_action('sl_after_content', 'sl_after_content_setup');
+
+/**
+ *      Enqueue scripts and styles.
+ */
+function streamline_enqueue() {
+  // Theme Stylesheet (currently points to stylesheet in the sass folder)
+  wp_enqueue_style( 'streamline-style' , get_stylesheet_uri('stylesheets/style.css') );
+}
+add_action( 'wp_enqueue_scripts' , 'streamline_enqueue' );
--- ntr-streamline/index.php
+++ ntr-streamline/index.php
@@ -3,20 +3,31 @@
 get_header();
 ?>
 
-<div class="primary-col">
-  <main class="site-main">
+<div id="primary" class="primary-col">
+  <main id="main" class="site-main">
+
     <?php
-/*
-    if we have posts:
-      then while we have posts:
-        for each post:
-          get the template part for content-(post type)
-      else, get the template part for content-none
-*/
-    ?>
+    			if ( have_posts() ) :
+    				/* Start the Loop */
+    				while ( have_posts() ) : the_post();
+    					/*
+    					 * Include the Post-Format-specific template for the content.
+    					 * If you want to override this in a child theme, then include a file
+    					 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
+    					 */
+    					get_template_part( 'template-parts/post/content', get_post_format() );
+    				endwhile;
+    				// the_posts_pagination( array(
+    				// 	'prev_text' => twentyseventeen_get_svg( array( 'icon' => 'arrow-left' ) ) . '<span class="screen-reader-text">' . __( 'Previous page', 'twentyseventeen' ) . '</span>',
+    				// 	'next_text' => '<span class="screen-reader-text">' . __( 'Next page', 'twentyseventeen' ) . '</span>' . twentyseventeen_get_svg( array( 'icon' => 'arrow-right' ) ),
+    				// 	'before_page_number' => '<span class="meta-nav screen-reader-text">' . __( 'Page', 'twentyseventeen' ) . ' </span>',
+    				// ) );
+    			else :
+    				get_template_part( 'template-parts/post/content', 'none' );
+    			endif;
+		?>
+
   </main>
 </div>
-<div class="secondary-col">
   <?php get_sidebar(); ?>
-</div>
 <?php get_footer();
